fe6a2655fe6bdc287beaa077a9f218240603227a
Mario Colop <mariocolop09@email.com>
2026-05-05 20:39:03 -0600

    rutas actualizadas para evitar fallos

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('welcome');
+})->name('welcome');
 
 Route::view('/login', 'auth.login')->name('login');
 Route::view('/register', 'auth.register')->name('register');
