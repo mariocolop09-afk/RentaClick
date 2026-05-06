e433a3cf06bce4e0b3a3bd83a5287571df755f94
Mario Colop <mariocolop09@email.com>
2026-05-05 20:42:31 -0600

    nombre de la pagina principal arreglado para evitar errores

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+    return view('home');
+})->name('home');
 
 Route::view('/login', 'auth.login')->name('login');
 Route::view('/register', 'auth.register')->name('register');
